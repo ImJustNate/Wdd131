f6273ddbee34d68854d5de527d486d0a6c53db44
ImJustNate <156931228+ImJustNate@users.noreply.github.com>
2024-11-25 13:37:24 -0700

    final

--- recipes/main.js
+++ recipes/main.js
@@ -1,70 +1,62 @@
+
 import recipes from './recipes.mjs';
 
-function RandomNum(num){
-    return Math.floor(Math.random() * num)
+function RandomNum(num) {
+    return Math.floor(Math.random() * num);
 }
 
-function RandomRecipe(list){
-	const randomNum = RandomNum(list.length);
-
-	return list[randomNum];
+function RandomRecipe(list) {
+    const randomNum = RandomNum(list.length);
+    return list[randomNum];
 }
 
 function ReturnTags(tags) {
     return tags.map(tag => `<button>${tag}</button>`).join('');
 }
 
-
-
-function ReturnStars(num){
-    const newSection = document.createElement("section");
-    if (num == 5){
-        newSection.innerHTML = `
+function ReturnStars(num) {
+    if (num >= 5) {
+        return `
         <span aria-hidden="true" class="icon-star">⭐</span>
         <span aria-hidden="true" class="icon-star">⭐</span>
         <span aria-hidden="true" class="icon-star">⭐</span>
         <span aria-hidden="true" class="icon-star">⭐</span>
-        <span aria-hidden="true" class="icon-star">⭐</span>`
+        <span aria-hidden="true" class="icon-star">⭐</span>`;
+    } else if (num >= 4) {
+        return `
+        <span aria-hidden="true" class="icon-star">⭐</span>
+        <span aria-hidden="true" class="icon-star">⭐</span>
+        <span aria-hidden="true" class="icon-star">⭐</span>
+        <span aria-hidden="true" class="icon-star">⭐</span>
+        <span aria-hidden="true" class="icon-star-empty">☆</span>`;
+    } else if (num >= 3) {
+        return `
+        <span aria-hidden="true" class="icon-star">⭐</span>
+        <span aria-hidden="true" class="icon-star">⭐</span>
+        <span aria-hidden="true" class="icon-star">⭐</span>
+        <span aria-hidden="true" class="icon-star-empty">☆</span>
+        <span aria-hidden="true" class="icon-star-empty">☆</span>`;
+    } else if (num >= 2) {
+        return `
+        <span aria-hidden="true" class="icon-star">⭐</span>
+        <span aria-hidden="true" class="icon-star">⭐</span>
+        <span aria-hidden="true" class="icon-star-empty">☆</span>
+        <span aria-hidden="true" class="icon-star-empty">☆</span>
+        <span aria-hidden="true" class="icon-star-empty">☆</span>`;
+    } else if (num >= 1) {
+        return `
+        <span aria-hidden="true" class="icon-star">⭐</span>
+        <span aria-hidden="true" class="icon-star-empty">☆</span>
+        <span aria-hidden="true" class="icon-star-empty">☆</span>
+        <span aria-hidden="true" class="icon-star-empty">☆</span>
+        <span aria-hidden="true" class="icon-star-empty">☆</span>`;
     }
-    else if (num == 4){
-        newSection.innerHTML = `
-        <span aria-hidden="true" class="icon-star">⭐</span>
-        <span aria-hidden="true" class="icon-star">⭐</span>
-        <span aria-hidden="true" class="icon-star">⭐</span>
-        <span aria-hidden="true" class="icon-star">⭐</span>
-        <span aria-hidden="true" class="icon-star-empty">☆</span>`
-    }
-    else if (num == 3){
-        newSection.innerHTML = `
-        <span aria-hidden="true" class="icon-star">⭐</span>
-        <span aria-hidden="true" class="icon-star">⭐</span>
-        <span aria-hidden="true" class="icon-star">⭐</span>
-        <span aria-hidden="true" class="icon-star-empty">☆</span>
-        <span aria-hidden="true" class="icon-star-empty">☆</span>`
-    }
-    else if (num == 2){
-        newSection.innerHTML = `
-        <span aria-hidden="true" class="icon-star">⭐</span>
-        <span aria-hidden="true" class="icon-star">⭐</span>
-        <span aria-hidden="true" class="icon-star-empty">☆</span>
-        <span aria-hidden="true" class="icon-star-empty">☆</span>
-        <span aria-hidden="true" class="icon-star-empty">☆</span>`
-    }
-    if (num == 1){
-        newSection.innerHTML = `
-        <span aria-hidden="true" class="icon-star">⭐</span>
-        <span aria-hidden="true" class="icon-star-empty">☆</span>
-        <span aria-hidden="true" class="icon-star-empty">☆</span>
-        <span aria-hidden="true" class="icon-star-empty">☆</span>
-        <span aria-hidden="true" class="icon-star-empty">☆</span>`
-    }
-    return newSection
 }
 
-function htmlTemplate(item){
-        return `<section class="recipeCard">
-            <img src="${item.image}" alt="Image of ${item.name} on a plate">
-            <div>
+function htmlTemplate(item) {
+    return `<section class="recipeCard">
+        <img src="${item.image}" alt="Image of ${item.name} on a plate">
+        <div>
             ${ReturnTags(item.tags)}
             <h2>${item.name}</h2>
             <section>
@@ -76,37 +68,22 @@ function htmlTemplate(item){
                 ${ReturnStars(item.rating)}
                 </span>
             </section>
-            <p>
-            ${item.description}
-            </p>
-            </div>
-        </section>`;
+            <p>${item.description}</p>
+        </div>
+    </section>`;
+}
+
+function appendBookCards(item) {
+
     
-}
-console.log(ReturnTags(['dessert', 'German']))
-console.log(recipes[0].tags)
-console.log(htmlTemplate(RandomRecipe(recipes)))
+    const titleElement = document.getElementById("here");
+    titleElement.innerHTML = htmlTemplate(item);
 
-function renderRecipes(recipeList) {
-	// get the element we will output the recipes into
-
-        const outputElement = document.getElementsByName('main');
-	// use the recipeTemplate function to transform our recipe objects into recipe HTML strings
-        const recipesHTML = recipeList.map(htmlTemplate).join('');
-	// Set the HTML strings as the innerHTML of our output element.
-        outputElement.innerHTML = recipesHTML;
-
+    // const newSection = document.getElementsByClassName("recipeCard");
+    // newSection.innerHTML = htmlTemplate(item);
+    // document.body.appendChild(newSection);
 }
 
-function init() {
-  // get a random recipe
-  const recipe = RandomRecipe(recipes)
-  // render the recipe with renderRecipes.
-  renderRecipes([recipe]);
-}
-init();
+const randomRecipe = RandomRecipe(recipes);
+appendBookCards(randomRecipe);
 
-// document.body.appendChild(htmlTemplate(RandomRecipe(recipes)));
-
-
-console.log(RandomRecipe(recipes))
